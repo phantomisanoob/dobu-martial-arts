2a5bb166e966654b8bd62195de97875704bcf7ea
DIOGOA-PHA <Da111617@hc.ac.je>
2023-04-28 14:38:51 +0100

    fixing log out

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import logo from "~/Assets/Svg/DoBuLogo.jpg";
 import NavBarLink from "./NavbarLink";
 import { FacebookIcon, FacebookShareButton } from "react-share";
+import { Form, Link } from "@remix-run/react";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(true);
@@ -52,7 +53,24 @@ const NavBar = () => {
         <NavBarLink url="/about">About Us</NavBarLink>
         <NavBarLink url="/classes">Classes</NavBarLink>
         <NavBarLink url="/membership">Membership</NavBarLink>
-        <NavBarLink url="/login">Login</NavBarLink>
+        {user ? (
+          <Form action="/logout" method="post">
+            <button
+              type="submit"
+              className="mt-4 rounded border-2 border-slate-700 bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600 lg:mt-0 lg:ml-4"
+            >
+              Logout
+            </button>
+          </Form>
+        ) : (
+          <Link
+            to="/login"
+            type="button"
+            className="mt-4 rounded border-2 border-neutral-700 bg-neutral-300 px-4 py-2 text-neutral-900 transition-colors hover:bg-neutral-100 lg:mt-0 lg:ml-4"
+          >
+            Login
+          </Link>
+        )}
       </div>
     </header>
   );
